9cb900523ff6db5a37ea050293a9aa098148ba61
Ivan Rios <ivars31@gmail.com>
2015-09-25 18:22:59 -0400

    Fix to tripDetection algorithm

--- blizzard/tripDetection.py
+++ blizzard/tripDetection.py
@@ -1,10 +1,9 @@
-__author__ = 'irios'
 from math import pow, sqrt
-from Parameter import Parameter
+from parameter import Parameter
 from Data import Data
-from trajectory import get_trajectory_information
+import trajectory
 
-def calculate_effective_magnitude(data):
+def calculate_module(data):
     sq_sum = 0
     for i in data:
         sq_sum += pow(i, 2)
@@ -13,24 +12,20 @@ def calculate_effective_magnitude(data):
 class TripDetection:
     def __init__(self):
         # Parameters
-        self.CHARGING_CURRENT_LIMIT = 4.8
-        self.GYRO_LIMIT = 0.09
-        self.LIN_ACCEL_LIM = 1
-
-        self.MIN_NUMBER_OF_VALUES_OVER_LIMIT = 4
-        self.ARRAY_LENGTH = 12
-        self.MIN_TRIP_LENGTH = 300 #s
-        self.TRIP_STARTED = False
-        self.MAX_TIME_BETWEEN_MOVEMENT = 280 #s
-        self.MAX_AVG_SPEED = 30 #km/h
+        self.charging_current_limit = 5
+        self.gyro_limit = 0.15
+        self.min_number_of_values_over_limit = 25
+        self.array_length = 90
+        self.min_trip_length = 300 #s
+        self.trip_started = False
+        self.max_time_between_movement = 280 #s
+        self.max_avg_speed = 30 #km/h
 
         # Declaring variables
-        self.charging_currents = Parameter(self.ARRAY_LENGTH, self.CHARGING_CURRENT_LIMIT, self.MIN_NUMBER_OF_VALUES_OVER_LIMIT)
-        self.gyroscopes = Parameter(self.ARRAY_LENGTH, self.GYRO_LIMIT, self.MIN_NUMBER_OF_VALUES_OVER_LIMIT)
-        self.lin_accels = Parameter(self.ARRAY_LENGTH, self.LIN_ACCEL_LIM, self.MIN_NUMBER_OF_VALUES_OVER_LIMIT)
+        self.charging_currents = Parameter(self.array_length, self.charging_current_limit, self.min_number_of_values_over_limit)
+        self.gyroscopes = Parameter(self.array_length, self.gyro_limit, self.min_number_of_values_over_limit)
         self.start_times = []
         self.end_times = []
-        self.distances = []
 
         # Declaring temporary variable for trips
         self.tmp_start_time = 0
@@ -43,129 +38,66 @@ class TripDetection:
         self.gyro_x_db = 13
         self.gyro_y_db = 14
         self.gyro_z_db = 15
-        self.lin_accel_x_db = 19
-        self.lin_accel_y_db = 20
-        self.lin_accel_z_db = 21
 
     def detect_trips(self, dbc, imei, start_date, end_date):
         # Obtaining all the data from the DB
-        query = "select * from imei{0} where stamp >= \"{1}\" and stamp <= \"{2}\" and gyrox <> 0 and gyroy <> 0 and gyroz <> 0 order by stamp".format(imei, start_date, end_date)
+        query = "select * from imei{0} where stamp >= \"{1}\" and stamp <= \"{2}\" order by stamp".format(imei, start_date, end_date)
         for record in dbc.SQLSelectGenerator(query):
-
             # Setting the data required for the algorithm
-            # Setting the charging current
             if record[self.charging_current_db] is not None:
                 charging_current = Data(record[self.time_db], record[self.charging_current_db])
                 self.charging_currents.add_data(charging_current)
-            # Setting the gyroscope
             gyro_x = record[self.gyro_x_db]
             gyro_y = record[self.gyro_y_db]
             gyro_z = record[self.gyro_z_db]
             if gyro_x is not None and gyro_y is not None and gyro_z is not None:
-                gyro_magnitude = calculate_effective_magnitude([gyro_x, gyro_y, gyro_z])
-                gyro = Data(record[self.time_db], gyro_magnitude)
+                gyro_module = calculate_module([gyro_x, gyro_y, gyro_z])
+                gyro = Data(record[self.time_db], gyro_module)
                 self.gyroscopes.add_data(gyro)
-            # Setting the linear acceleration
-            lin_accel_x = record[self.lin_accel_x_db]
-            lin_accel_y = record[self.lin_accel_y_db]
-            lin_accel_z = record[self.lin_accel_z_db]
-            if lin_accel_x is not None and lin_accel_y is not None and lin_accel_z is not None:
-                lin_accel_magnitude = calculate_effective_magnitude([lin_accel_x, lin_accel_y, lin_accel_z])
-                lin_accel = Data(record[self.time_db], lin_accel_magnitude)
-                self.lin_accels.add_data(lin_accel)
 
-            # If trip has not started we check the battery is not charging and gyroscope or
-            # linear accelerometer have detected movement
-            if not self.TRIP_STARTED:
-                if not self.charging_currents.max_limit_reached() and (self.gyroscopes.max_limit_reached() or self.lin_accels.max_limit_reached()):
-                    if self.gyroscopes.get_min_time_over_limit():
-                        self.start_trip(self.gyroscopes.get_min_time_over_limit())
-                    elif self.lin_accels.get_min_time_over_limit():
-                        self.start_trip(self.lin_accels.get_min_time_over_limit())
+            # If trip has not started we check current and
+            if not self.trip_started:
+                if not self.charging_currents.max_limit_reached() and self.gyroscopes.max_limit_reached():
+                    print("trip started:", record[self.time_db])
+                    self.start_trip(self.gyroscopes.get_min_time_over_limit())
             # Case when trip has started
             else:
-                if self.charging_currents.max_limit_reached() or (not self.gyroscopes.max_limit_reached() and not self.lin_accels.max_limit_reached()):
+                if self.charging_currents.max_limit_reached() or not self.gyroscopes.max_limit_reached():
+                    print("trip ended:", record[self.time_db])
                     self.end_trip(record[self.time_db])
-        self.calculate_distances(dbc, imei)
-        if len(self.start_times) > 1:
-            self.merge_trips()
         self.validate_all_trips(dbc,imei)
-        return self.start_times, self.end_times, self.distances
+        return self.start_times, self.end_times
 
-    def calculate_distances(self, dbc, imei):
-        BETA = 0.06
+    def validate_all_trips(self, dbc, imei):
+        beta = 0.06
         for i, j in zip(self.start_times, self.end_times):
             time = (j - i).total_seconds()/3600
-            _, _, _, _,distance, _, _ = get_trajectory_information(dbc, imei, BETA, [i], [j])
+            _, _, _, _,distance, _, _ = trajectory.get_trajectory_information(dbc, imei, beta, [i], [j])
             if distance:
-                self.distances.append(distance[0])
-            else:
-                self.distances.append(0)
-
-    def merge_trips(self):
-        tmp_start_times = []
-        tmp_end_times = []
-        tmp_distances = []
-        merged = False
-        for i in range(len(self.start_times) - 1):
-            if (self.start_times[i + 1] - self.end_times[i]).total_seconds() < self.MAX_TIME_BETWEEN_MOVEMENT:
-                # Merging if the trips are close in time (limit defined previously)
-                # If the previous record was not merged, just add it to the lists of data
-                if not merged:
-                    tmp_start_times.append(self.start_times[i])
-                    tmp_end_times.append(self.end_times[i + 1])
-                    tmp_distances.append(self.distances[i] + self.distances[i + 1])
-                # If the previous record was merged, modify the last record of the new lists of data (end time and dist)
-                else:
-                    tmp_end_times[-1] = self.end_times[i + 1]
-                    tmp_distances[-1] = tmp_distances[-1] + self.distances[i + 1]
-                # Saving the state as merged
-                merged = True
-            else:
-                if not merged:
-                    tmp_start_times.append(self.start_times[i])
-                    tmp_end_times.append(self.end_times[i])
-                    tmp_distances.append(self.distances[i])
-                # Saving the state as not merged
-                merged = False
-            if i == len(self.start_times) - 1 and not merged:
-                # If it is the last element in the list and it was not merged, add it to the new list
-                tmp_start_times.append(self.start_times[-1])
-                tmp_end_times.append(self.end_times[-1])
-                tmp_distances.append(self.distances[-1])
-        # Replace the temporary data in the data of the object
-        self.start_times = tmp_start_times
-        self.end_times = tmp_end_times
-        self.distances = tmp_distances
-
-    # Validate that duration, avg speed and distance are within the limits
-    def validate_all_trips(self, dbc, imei):
-        tmp_start_times = []
-        tmp_end_times = []
-        tmp_distances = []
-        for start_time, end_time, distance in zip(self.start_times, self.end_times, self.distances):
-            time = (end_time - start_time).total_seconds()/3600
-            speed = distance / time
-            # In case when trip duration is longer than stablished AND avg speed and distances are over the limits
-            # also, when distance is 0, the trip is saved because it means there is no GPS data available
-            if ((distance > 0 and speed < self.MAX_AVG_SPEED) or distance == 0) and self.validate_trip_duration(start_time, end_time) and time < 2:
-                tmp_distances.append(distance)
-                tmp_start_times.append(start_time)
-                tmp_end_times.append(end_time)
-        # Replace the temporary data in the data of the object
-        self.start_times = tmp_start_times
-        self.end_times = tmp_end_times
-        self.distances = tmp_distances
+                speed = distance[0] / time
+                if speed > self.max_avg_speed:
+                    self.start_times.remove(i)
+                    self.end_times.remove(j)
 
     def start_trip(self, start_time):
-        self.TRIP_STARTED = True
+        self.trip_started = True
         self.tmp_start_time = start_time
 
     def end_trip(self, end_time):
-        self.TRIP_STARTED = False
-        self.start_times.append(self.tmp_start_time)
-        self.end_times.append(end_time)
+        self.trip_started = False
+        if self.validate_trip(end_time):
+            # If there is at least one value saved, then check that trips do not overlap
+            # If they overlap then join the trips together
+            if len(self.end_times) > 0:
+                if (self.tmp_start_time - self.end_times[-1]).total_seconds() > self.max_time_between_movement:
+                    self.start_times.append(self.tmp_start_time)
+                    self.end_times.append(end_time)
+                else:
+                    self.end_times[-1] = end_time
+            else:
+                self.start_times.append(self.tmp_start_time)
+                self.end_times.append(end_time)
         self.tmp_start_time = 0
 
-    def validate_trip_duration(self, start_time, end_time):
-        return (end_time - start_time).total_seconds() > self.MIN_TRIP_LENGTH
+    def validate_trip(self, end_time):
+        return (end_time - self.tmp_start_time).total_seconds() > self.min_trip_length
